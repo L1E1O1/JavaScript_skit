ae53c8f33f4f788cd3a56709f224f9614d7d0bd4
Sajad Bayram <sajad.bayram@learnet.se>
2024-11-15 10:11:18 +0100

    fixa skiten

--- noteapp/java.js
+++ noteapp/java.js
@@ -1,93 +1,79 @@
-function updateStorage(curNotes){
-    localStorage.setItem(curNotes.dataset.noteNr, curNotes.innerText);
-    console.log("Storage updated");
-    console.log(localStorage.getItem("notes-"+curNotes.dataset.noteNr));
-}
-
-
-function showNotes() {
-    const notes = Object.keys(localStorage);
-    const highestAmountOfNotes = Math.max(amountOfNotes);
-console.log("det här är största nummret" + highestAmountOfNotes);
-
-    
-for (let i = 1; i < amountOfNotes.length; i++) {
-    if ( amountOfNotes[i] > highestAmountOfNotes) {
-      highestAmountOfNotes = numbers[i];
-    }
-  }
-    notes.forEach(function(note){
-        amountOfNotes++;
-        //KOLLA VILKEN SOM ÄR DET HÖGST NOTES-SIFFRAN OCH GÖR amountOfNotes till den + 1
-        console.log(amountOfNotes);
-        let inputBox = document.createElement("p");
-        inputBox.innerText = localStorage.getItem(note);
-        inputBox.dataset.noteNr =  note;
-        inputBox.className = "input-box";
-        inputBox.setAttribute("contenteditable", "true");
-        let img = document.createElement("img");
-        img.src = "/noteapp/images/delete.jpg";   
-
-        inputBox.appendChild(img);
-        notesContainer.appendChild(inputBox);
-       // console.log(localStorage.getItem(note));
-    });
- 
- }
 var notesContainer;
 var createBtn;
 var amountOfNotes = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
     notesContainer = document.querySelector(".container");
     createBtn = document.querySelector(".btn");
-    createBtn.addEventListener('click', function () {
-        amountOfNotes++;
-        
 
+    function updateStorage(curNotes) {
+        localStorage.setItem(curNotes.dataset.noteNr, curNotes.innerText);
+        console.log("Storage updated");
+        console.log(localStorage.getItem(curNotes.dataset.noteNr));
+    }
 
+    function findHighestNoteNumber() {
+        let highestNoteNumber = 0;
+        for (const key of Object.keys(localStorage)) {
+            const noteNumber = parseInt(key.replace("Notes-", ""), 10);
+            if (!isNaN(noteNumber) && noteNumber > highestNoteNumber) {
+                highestNoteNumber = noteNumber;
+            }
+        }
+        return highestNoteNumber;
+    }
+
+    function showNotes() {
+        amountOfNotes = findHighestNoteNumber() + 1;
+        const notes = Object.keys(localStorage);
+        notes.forEach(function (note) {
+            let inputBox = document.createElement("p");
+            inputBox.innerText = localStorage.getItem(note);
+            inputBox.dataset.noteNr = note;
+            inputBox.className = "input-box";
+            inputBox.setAttribute("contenteditable", "true");
+
+            let img = document.createElement("img");
+            img.src = "/noteapp/images/delete.jpg";
+
+            inputBox.appendChild(img);
+            notesContainer.appendChild(inputBox);
+        });
+    }
+
+    createBtn.addEventListener("click", function () {
+        let newNoteNumber = amountOfNotes++;
         let inputBox = document.createElement("p");
-        inputBox.dataset.noteNr = "Notes-" + amountOfNotes;
+        inputBox.dataset.noteNr = "Notes-" + newNoteNumber;
         inputBox.className = "input-box";
         inputBox.setAttribute("contenteditable", "true");
+
         let img = document.createElement("img");
-        img.src = "/noteapp/images/delete.jpg";  
+        img.src = "/noteapp/images/delete.jpg";
 
         inputBox.appendChild(img);
         notesContainer.appendChild(inputBox);
         updateStorage(inputBox);
     });
 
-
-    notesContainer.addEventListener('click', function(del){
-        console.log(del.target.parentElement);
-        if(del.target.tagName === "IMG"){
+    notesContainer.addEventListener("click", function (del) {
+        if (del.target.tagName === "IMG") {
             del.target.parentElement.remove();
-            localStorage.removeItem("notes-"+del.target.parentElement.dataset.noteNr);
+            localStorage.removeItem(del.target.parentElement.dataset.noteNr);
         }
-        if(del.target.tagName === "P"){
-            notes = document.querySelectorAll(".input-box");
-            notes.forEach(par => {
-                par.onkeyup = function(){
-                    updateStorage(del.target);
-                }
-            })
+        if (del.target.tagName === "P") {
+            del.target.onkeyup = function () {
+                updateStorage(del.target);
+            };
         }
-    })
+    });
 
-    document.addEventListener("keydown", event =>{
-        if(event.key === "Enter"){
-            document.execCommand("insertLineBreak")
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            document.execCommand("insertLineBreak");
             event.preventDefault();
-            //updateStorage();
         }
-    })
-    /**
- * potentiall solutions 
- * array to sort array of notes based on the last modification date????????
- * interface to store notes in a more secure way and easy way to retrieve them and delete them????????
- * har fixat typ så att den kan känna igen största notes-siffra
- */
-     
-  showNotes();
-});
+    });
 
+    showNotes();
+});
