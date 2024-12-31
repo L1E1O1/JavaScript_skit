7b8ad7d5079841adb263fdc96583fed3ffc625d0
Sajad <sajadbay47@gmail.com>
2024-12-31 11:33:59 +0100

    made some changes

--- logInForm/script.js
+++ logInForm/script.js
@@ -1,61 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-let username = document.getElementById("username");
-let password = document.getElementById("password");
-let checkPassword = document.getElementById("checkPassword");
-let logIn = document.getElementById("submit");
-let match;
+document.addEventListener("DOMContentLoaded", function () {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let checkPassword = document.getElementById("checkPassword");
+  let logIn = document.getElementById("submit");
+  let match;
 
-
-
-logIn.addEventListener("click", function() {
+  logIn.addEventListener("click", function () {
     console.log(username + " " + password);
     usernameValidation();
     passwordValidation();
     revisePassword();
+  });
+  //check if passwords match
+  function revisePassword() {
 
-});
-
-function revisePassword(){
-
-
-
-    if(username.Value === undefined){
-    alert("Please enter a username");
-    console.log("username is empty")
+    if (password.value !== checkPassword.value) {
+      alert("passord does not match");
+    } else {
+      match = password.value === checkPassword.value;
+      console.log(match);
     }
+  }
 
-    else if (password.value !== checkPassword.value) {
-        alert("passord does not match")
-}else {
-match = (password.value === checkPassword.value)
-console.log(match);
-}
-}
+  //validate password length and check password value
+  function passwordValidation() {
 
 
-function passwordValidation(){
- if (password.length >= 8 && password.length <= 16) {
-    console.log("Password length is correct");
-}
-    else {
-        alert("Password length should be between 8 and 16 characters");
-        password.focus();
-        return false;
+    if (password.length >= 8 && password.length <= 16) {
+      console.log("Password length is correct");
+    } else {
+      alert("Password length should be between 8 and 16 characters");
+      console.log(password.length)
+      return false;
     }
+  }
 
-}
-
-
-function usernameValidation(){
+  // no symbols can be entered && checks if username is empty
+  function usernameValidation() {
     let regex = /^[a-zA-Z0-9]+$/;
-
-    if(username.value === regex) {
-        console.log("Username is valid");
+    
+    
+    if (username.value === regex) {
+      console.log("Username is valid");
+    } else {
+      alert("Username should only contain alphanumeric characters");
+      return false;
     }
-    else {
-        alert("Username should only contain alphanumeric characters");
-        username.focus();
-        return false;
-    }
-}
+  }
 });
